2018d3ee8d8a210bbc410c2a1360cfa77d49bc0e
wclyffe <wclyffe@server.fake>
2014-06-18 18:53:42 +0200

    Use sh_util.make_archive to build foo-package.zip

--- package.py
+++ package.py
@@ -1,3 +1,35 @@
 # packaging script
+import os
 import shutil
-print('test')
+
+# High-level utilities to create and read compressed and archived files are also provided. They rely on the zipfile and tarfile modules.
+#
+# shutil.make_archive(base_name, format[, root_dir[, base_dir[, verbose[, dry_run[, owner[, group[, logger]]]]]]])
+#
+#     Create an archive file (such as zip or tar) and return its name.
+#
+#     base_name is the name of the file to create, including the path, minus any format-specific extension.
+#
+#     format is the archive format: one of “zip”, “tar”, “bztar” (if the bz2 module is available) or “gztar”.
+#
+#     root_dir is a directory that will be the root directory of the archive;
+#              for example, we typically chdir into root_dir before creating the archive.
+#
+#     base_dir is the directory where we start archiving from; i.e. base_dir will be the common prefix of all files and directories in the archive.
+#
+#     root_dir and base_dir both default to the current directory.
+#
+#     owner and group are used when creating a tar archive. By default, uses the current owner and group.
+#
+#     logger must be an object compatible with PEP 282, usually an instance of logging.Logger.
+
+
+current_dir = os.path.dirname(__file__)
+addon_dir = os.path.join(current_dir, 'foo-package')
+
+archive_name = shutil.make_archive(base_name='foo-package',
+                                   format="zip",
+                                   root_dir=current_dir,
+                                   base_dir=addon_dir)
+
+print(archive_name)
